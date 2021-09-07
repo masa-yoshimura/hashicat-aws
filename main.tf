4bb7edb47c7349f7faecb9c503117b5b9aaffebe
Masashi Yoshimura <masashi.yoshimura@ctc-g.co.jp>
2021-09-07 02:53:00 +0000

    Added the first tag

--- main.tf
+++ main.tf
@@ -116,7 +116,6 @@ data "aws_ami" "ubuntu" {
 resource "aws_eip" "hashicat" {
   instance = aws_instance.hashicat.id
   vpc      = true
-  Department = devops
 }
 
 resource "aws_eip_association" "hashicat" {
